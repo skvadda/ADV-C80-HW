c0e49843c228fad42074befac1786ce6bd932b51
skvadda <157056655+skvadda@users.noreply.github.com>
2024-01-18 13:42:31 -0500

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var names_of_people = [];
 function submit()
 {
     var GuestName = document.getElementById("name1").value;
-	// use the push function as - names_of_people.push(GuestName);
+	names_of_people.push(GuestName);
 	console.log(GuestName);    
     console.log(names_of_people);
     var lenght_of_name = names_of_people.length;
@@ -25,8 +25,7 @@ function show()
 
 function sorting()
 	{
-		names_of_people           // add the sort function here
-		// .sort();
+		names_of_people.sort();
 		var i= names_of_people.join("<br>");
 		console.log(names_of_people);		
 		document.getElementById("sorted").innerHTML=i.toString();
